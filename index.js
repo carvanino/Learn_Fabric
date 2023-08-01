27bf84a829f28a589d72c22723699340486568b2
Akinola <akinolatofunmi04@gmail.com>
2023-08-01 18:43:17 +0100

    Making operations into modes

--- index.js
+++ index.js
@@ -29,28 +29,50 @@ const setBackground = (url, canvas) => {
 const canvas = initCanvas('canvas');
 let mousePressed = false;
 const imgaeURL = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'
+
+let currentMode;
+modes = {
+    pan: 'pan',
+}
 setBackground(imgaeURL, canvas);
 
-canvas.on('mouse:move', (event) => {
-    // console.log(event);
-    if (mousePressed) {
-        canvas.setCursor('grab');
-        const mEvent = event.e;
-        console.log(mEvent.movementX, mEvent.movementY);
-        const delta = new fabric.Point(mEvent.movementX, mEvent.movementY);
-        canvas.relativePan(delta);
+const togglePan = () => {
+    const panButton = document.getElementById('pan');
+    
+    // panButton.setAttribute("color", "red")
+    if (currentMode === modes.pan) {
+        panButton.style.backgroundColor = "";
+        currentMode = '';
+    } else {
+        panButton.style.backgroundColor = "cyan";
+        currentMode = modes.pan;
     }
-});
+}
+const setPanEvent = (canvas) => {
+    canvas.on('mouse:move', (event) => {
+        // console.log(event);
+        if (mousePressed && currentMode == modes.pan) {
+            canvas.setCursor('grab');
+            const mEvent = event.e;
+            // console.log(mEvent.movementX, mEvent.movementY);
+            const delta = new fabric.Point(mEvent.movementX, mEvent.movementY);
+            canvas.relativePan(delta);
+        }
+    });
+    
+    canvas.on('mouse:up', (event) => {
+        mousePressed = false;
+    });
+    
+    canvas.on('mouse:down', (event) => {
+        mousePressed = true;
+        if (currentMode === modes.pan) {
+            canvas.setCursor('grab');
+            canvas.renderAll();
+        }
+    });
 
-canvas.on('mouse:up', (event) => {
-    mousePressed = false;
-})
-
-canvas.on('mouse:down', (event) => {
-    mousePressed = true;
-    canvas.setCursor('grab');
-    canvas.renderAll();
-
-})
+}
+setPanEvent(canvas);
 
 canvas.renderAll();
